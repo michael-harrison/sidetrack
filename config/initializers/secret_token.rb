5fb4e8e0eae854b8f81540e91fce0556ffb0661e
Michael Harrison <michael@focalpause.com>
2011-08-06 23:10:51 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sidetrack::Application.config.secret_token = '72c20b4b1cb54dd39fcae6d226d4066dc878dcd51b894405d83c846853b5a2a3286b163e730c867f7c46668957a1586bb46807648b15f445e98ecdf2e6200667'
